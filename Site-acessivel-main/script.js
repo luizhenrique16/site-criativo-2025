8b6a07f01671dd970a34b98ebae80e174a6df482
luiz lindo <142804881+luizhenrique16@users.noreply.github.com>
2025-03-24 12:23:28 +0000

    luiz

--- Site-acessivel-main/script.js
+++ Site-acessivel-main/script.js
@@ -1,11 +1,17 @@
 document.addEventListener('DOMContentLoaded', function(){
     const aumentaFonteBotao = document.getElementById('aumentar-fonte');
-    const diminuiFonteBotao = document.getElementById9()
+    const diminuiFonteBotao = document.getElementById9('diminuir-fonte');
 
     let tamanhoAtualFonte = 1;
 
-    aumentaFonteBotao.addEventListener('click'; function(){
+    aumentaFonteBotao.addEventListener('click', function(){
         tamanhoAtualFonte += 0.1;
+        document.body.style.fontSize = `${tamanhoAtualFonte}rem`
+
+    })
+
+    diminuiFonteBotao.addEventListener('click', function(){
+        tamanhoAtualFonte -= 0.1;
         document.body.style.fontSize = `${tamanhoAtualFonte}rem`
 
     })
