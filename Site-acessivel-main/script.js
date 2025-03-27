560b17ec2ba906460d8a3a1844fa205776f48fab
luiz lindo <142804881+luizhenrique16@users.noreply.github.com>
2025-03-27 12:30:22 +0000

    luiz llll

--- Site-acessivel-main/script.js
+++ Site-acessivel-main/script.js
@@ -1,4 +1,12 @@
 document.addEventListener('DOMContentLoaded', function(){
+   const botaoDeAcessibilidade = document.getElementById('botao-acessibilidade')
+   const opçoesDeAcessibilidade = document.getElementById('opçoes-acessibilidade')
+
+   botaoDeAcessibilidade.addEventListener('click', function (){
+    botaoDeAcessibilidade.classList.toggle('rotaçao-botao');
+    opçoesDeAcessibilidade.classList.toggle('apresenta-lista')
+   })
+ 
     const aumentaFonteBotao = document.getElementById('aumentar-fonte');
     const diminuiFonteBotao = document.getElementById('diminuir-fonte');
 
